ec288b7f5f9cc446a1f28c4eae537b80c068355b
cate97819 <kate.pfo@gmail.com>
2024-12-17 09:38:57 +0900

    20241217

--- components/section/MainVisual.tsx
+++ components/section/MainVisual.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MainVisual = () => {
   return (
-    <div className='min-h-[600px] w-full bg-green-50 text-white flex flex-col items-center justify-center'>
+    <div className='min-h-[600px] w-full bg-slate-200 text-white flex flex-col items-center justify-center'>
       <img src="/images/logo.png" alt="" className='w-full max-w-[800px] px-4'/>
     </div>
   )
